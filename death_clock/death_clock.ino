6e1a61cb8aa4249edabddf994ebeb2056ce72810
Adam Tait <adam@adamtait.com>
2017-11-10 17:49:33 -0800

    add EEPROM storage of current epoch time, every 10 minutes

--- death_clock/death_clock.ino
+++ death_clock/death_clock.ino
@@ -1,24 +1,37 @@
 #include <LiquidCrystal.h>
 #include <TimeLib.h>
+#include <EEPROM.h>
 #include <math.h>
 
+
+// THESE TWO CONSTANTS NEED TO BE SET
+#define CURRENT_EPOCH_TIME_SECONDS      1510359495    // IMPORTANT: update this number to the current epoch time, each time the board is restarted.
+#define FINAL_EPOCH_TIME_SECONDS        2895868800    // this is the expected epoch time of your life expectancy
+
+
+
+
+
+
+
+
+#define eeprom_addr 0
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 
-// IMPORTANT: update this number to the current epoch time, each time the board is restarted.
-#define CURRENT_EPOCH_TIME_SECONDS  1510358835
-#define FINAL_EPOCH_TIME_SECONDS    2895868800
 
 
 void setup() 
 {
-  setTime(CURRENT_EPOCH_TIME_SECONDS);
   lcd.begin(16, 2);
+  
+  // SET TIME
+  long stored_epoch_time = 0;
+  EEPROM.get( eeprom_addr, stored_epoch_time );
 
-  lcd.noDisplay();
-  lcd.display();
-
-//  Serial.begin(9600);   // for debugging
+  long t = CURRENT_EPOCH_TIME_SECONDS;
+  if ( stored_epoch_time > t )  { t = stored_epoch_time; }
+  setTime( t );   // used by now()
 }
 
 
@@ -30,9 +43,13 @@ void loop()
 
   printToLcd(0, formatLong(years(diff), 5) + " years");
   printToLcd(1, printFormat(days(diff), hours(diff), minutes(diff), seconds(diff)));
+
+  // store current time in EEPROM
+  if ( current_epoch_time % (60 * 10) == 0 )  {   // only store time every 10 minutes. EEPROM has a limit to lifetime writes. What does 10 minutes matter in the whole scheme of your life, anyways?
+    EEPROM.put( eeprom_addr, current_epoch_time ); 
+  }       
   
   delay(1000);
-  
 }
 
 
